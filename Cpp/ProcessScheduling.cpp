86a0a5a8a2812219085fd12518ce08a81466f3b1
Albert Millert <devmood.business@gmail.com>
2017-11-17 18:42:57 +0100

    Added connection between ProcessesManager class and ProcessScheduler

--- Cpp/ProcessScheduling.cpp
+++ Cpp/ProcessScheduling.cpp
@@ -1,44 +1,32 @@
 #include "ProcessScheduling.h"
 
-ProcessScheduler::Process::Process(int ID, int arrivalTime, int burstTime) { //implementation of the Process's constructor
-	this->ID = ID;
-	this->arrivalTime = arrivalTime;
-	this->burstTime = burstTime;
+void ProcessScheduler::GetReadyProcessesList() {
+	readyProcesses = pm.GiveReadyProcessesList();
 }
 
-void ProcessScheduler::GetProcesses() { //implementation of the GetProcesses method; due to the fact that not all the modules have been implemented yet values are being set here manually
-	listProcess.push_back(Process(1, 0, 7));
-	listProcess.push_back(Process(2, 1, 5));
-	listProcess.push_back(Process(3, 2, 3));
-	listProcess.push_back(Process(4, 3, 1));
-	listProcess.push_back(Process(5, 4, 2));
-	listProcess.push_back(Process(6, 5, 1));
-}
+//void SetProcesBurstTime(int newBurstTime);
+//int GetProcesBurstTime();
+
 
 void ProcessScheduler::SRTSchedulingAlgorithm() {
-	int timePassed = 0; //timePassed responsible for simmulating the process of time passing
-	int minBurstTime = 100; //minBurstTime stores minimal burst time of a process iteratorToMinElement; used for comparison with other burstTimes to find the minimal one
-	std::list<Process>::iterator iteratorToMinElement; //candidate process to be executed next
-	while (listProcess.size() > 0) { //while-loop looping as long as processes are availible inside ready-queue (in this example linked list)
-		std::cout << "Time passed:\t\t" << timePassed << "\n";
-		for (std::list<Process>::iterator it = listProcess.begin(); it != listProcess.end(); it++) { //for-loop based on std::list<Process>::iterators looping over all processes in ready-queue
-			if (it->arrivalTime <= timePassed && it->burstTime <= minBurstTime) { //if-statement which finds the next candidate process to be executed next
-				minBurstTime = it->burstTime; //storing the minimal burstTime (found so far) inside ready-queue 
-				iteratorToMinElement = it; //storing the iterator to the best candidate process (found so far)
+	std::list<Process*>::iterator iteratorToMinElement;
+	int currentProcessActualBurstTime = 0;
+	int minBurstTime = 100;
+	while (readyProcesses.size() > 1) { // > 1 poniewa¿ zawsze bêdzie wewn¹trz proces bezczynnoœci
+		for (std::list<Process*>::iterator it = readyProcesses.begin(); it != readyProcesses.end(); it++) {
+			if ((*it)->GetProcesBurstTime() <= minBurstTime) {
+				minBurstTime = (*it)->GetProcesBurstTime();
+				iteratorToMinElement = it;
 			}
 		}
 
-		std::cout << "Process being executed:\t\t" << iteratorToMinElement->ID << "\n";
-		iteratorToMinElement->burstTime--; //decreasing the burstTime value, since one unit of time has passed
-		timePassed++;
-		if (!iteratorToMinElement->burstTime) { //if-statement checking whether a current process being executed has already ended processing itself
-			std::cout << "Process: \t" << iteratorToMinElement->ID << "\tterminated at time:\t" << timePassed << "s" << std::endl;
-			listProcess.erase(iteratorToMinElement); //erasing the terminated process
-			minBurstTime = 100; //setting minBurstTime to simmingly high value, since we don't know how high is the next minimal value for next process to be executed 
-		}
+		//Wywo³anie funkcji GoProcess(iteratorToMinElement) z wywo³aniami wszystkich wa¿nych funkcji
+		//Zmienianie wartoœci currentProcessActualBurstTime potrzebnej do obliczenia burst time po wykonaniu instrukcji
+		
+		
+		// this part should run when some instruction on process has been done in order to count new burstTime for process
+		(*iteratorToMinElement)->SetProcesBurstTime(.5 * currentProcessActualBurstTime + .5 * (*iteratorToMinElement)->GetProcesBurstTime); //ustawianie nowego czasu burstTime procesu
+		
+		//sprawdzanie czy proces jest skonczony; jeœli tak - zmieniæ mu stan lub po prostu go usun¹æ.
 	}
 }
-
-//	ProcessScheduler ps;
-//	ps.GetProcesses();
-//	ps.SRTSchedulingAlgorithm();
--- Headers/ProcessScheduling.h
+++ Headers/ProcessScheduling.h
@@ -1,23 +1,14 @@
 #pragma once
 #include <iostream>
 #include <list>
+#include "ProcessesManager.h"
 
-class ProcessScheduler { //class responsible for all the planning
+class ProcessScheduler {
+private:
+	std::list<Process*> readyProcesses;
+	void GetReadyProcessesList();
+	//jedynie na potrzeby implementacji (bo bêdzie chyba utworzone w mainie?):
+	ProcessesManager pm;
 public:
-	//Structures declarations:
-	struct Process { //structure representing the most basic concept of a process
-	public:
-		int ID, arrivalTime, burstTime; //ID stores an identificator of a process; arrivalTime stores the time when process has arrived in the system; burstTime stores a possible time of execution
-	public:
-		Process(int ID, int arrivalTime, int burstTime); //constructor with 3 parameters
-	};
-
-public:
-	//Variables declarations:
-	std::list<Process> listProcess; //listProcess std::list - ready-queue-like
-
-public:
-	// Methods declarations:
-	void GetProcesses(); //method responsible for pushing processes into ready-queue
 	void SRTSchedulingAlgorithm(); //implementation of the Shortest Remaining Time First Algorithm for scheduling processes
 };
